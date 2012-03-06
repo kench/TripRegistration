32c26ceda9c86933375e627c4539109536c06908
Kenley Cheung <ken@lordkenthegreat.com>
2012-03-06 13:03:57 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripRegistration::Application.config.secret_token = '14d62accc370be54443cae66cafacd599f4fd11f4de62cff39c76109e10c86728e0cef55544c936e074f0b51c8a71a0f823f314c0af6d7895d112cec676b4820'
